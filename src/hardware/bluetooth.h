bb513719962be35e0698a9b4ce8f7feb311de9bd
Andrew McDaniel <andrewmcdan@gmail.com>
2024-10-28 21:55:05 -0400

    Starting over on BT stuff.

--- src/hardware/bluetooth.h
+++ src/hardware/bluetooth.h
@@ -1,7 +1,5 @@
 #ifndef BLUETOOTH_H
 #define BLUETOOTH_H
-#include <gio/gio.h>
-#include <glib.h>
 #include <vector>
 #include <string>
 #include <thread>
@@ -36,16 +34,6 @@ class BTControl{
     std::vector<BTDevice> pairedDevices;
     std::vector<BTDevice> connectedDevices;
     std::vector<BTDevice> availableDevices;
-    GDBusConnection* connection;
-    void on_device_found(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_disappeared(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_changed(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_properties_changed(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_acquired(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_lost(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_error(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_unhandled(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_device_unhandled_error(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
 public:
     BTControl();
     ~BTControl();
@@ -74,17 +62,6 @@ class BTServices{
 public:
     BTServices();
     ~BTServices();
-    void on_characteristic_read(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_write(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_notify(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_changed(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_value_changed(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_properties_changed(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_acquired(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_lost(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_error(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_unhandled(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
-    void on_characteristic_unhandled_error(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data);
 };
 
 /**
@@ -95,7 +72,6 @@ class BTManager{
     BTControl* control;
     BTServices* services;
     std::jthread loopThread;
-    GMainLoop* gloop;
 public:
     BTManager();
     ~BTManager();
