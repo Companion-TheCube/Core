6f108bfdaccd14df8d6e35daa827a49cea354b1e
Andrew McDaniel <andrewmcdan@gmail.com>
2024-11-07 17:09:13 -0500

    Some work on mWave data handlers

--- src/hardware/mmWave.cpp
+++ src/hardware/mmWave.cpp
@@ -1,78 +1,305 @@
 #include "mmWave.h"
 
-std::vector<std::string> portNames;
-std::vector<std::string> portPrefixes;
-
 std::vector<uint8_t> commandMode = { 0xFD, 0xFC, 0xFB, 0xFA, 0x04, 0x00, 0xff, 0x00, 0x01, 0x00, 0x04, 0x03, 0x02, 0x01 };
 std::vector<uint8_t> commandModeAck = { 0xFD, 0xFC, 0xFB, 0xFA, 0x08, 0x00, 0xff, 0x01, 0x00, 0x00, 0x01, 0x00, 0x04, 0x03, 0x02, 0x01 };
 
-void generatePortNames();
+#ifdef _WIN32
+int wiringPiSetup() { return -1; }
+int serialOpen(const char* port, int baud) { return -1; }
+void serialPuts(int port, char* data) {}
+int serialDataAvail(int port) { return 0; }
+char serialGetchar(int port) { return 0; }
+#endif
 
 mmWave::mmWave()
 {
-    generatePortNames();
     if(wiringPiSetup() == -1){
         CubeLog::error("Failed to setup wiringPi.");
         return;
     }
-    this->readerThread = new std::jthread([&](std::stop_token st) {
-        int serialPort = serialOpen("/dev/ttyAMA4", 115200);
-        if(serialPort < 0){
+    this->readerThread = std::make_unique<std::jthread>([&](std::stop_token st) {
+        this->serialPort_h = serialOpen("/dev/ttyAMA4", 115200);
+        if(serialPort_h < 0){
             CubeLog::error("Failed to open serial port.");
             while(!st.stop_requested()){
                 genericSleep(1000);
                 CubeLog::info("Failure.");
             }
         }
-        // serialPuts(serialPort, (char*)commandMode.data());
+        // serialPuts(serialPort_h, (char*)commandMode.data());
         
         while(!st.stop_requested()){
-            genericSleep(1000);
-            while(serialDataAvail(serialPort) == 0){
-                genericSleep(1000);
+            Response response = readDataFrame();
+            if(response.success){
+                CubeLog::info("Data received: " + response.hexStr);
+                decodeDataFrame(response);
             }
-            std::string responseStr;
-            while(serialDataAvail(serialPort) > 0){
-                responseStr += serialGetchar(serialPort);
-            }
-            CubeLog::debugSilly("response length: " + std::to_string(responseStr.length()));
-            // create a vector of uint8_t from the string
-            std::vector<uint8_t> response(responseStr.begin(), responseStr.end());
-            // print the data as hex values
-            std::string hexStr;
-            std::ostringstream oss;
-            for(auto c: response){
-                oss << std::hex 
-                << std::setw(2) 
-                << std::setfill('0') 
-                << std::uppercase
-                << (int)c << " ";
-            }
-            hexStr = oss.str();
-            CubeLog::debugSilly("response: " + hexStr);
         }
     });
 }
 
 mmWave::~mmWave()
 {
-    delete this->readerThread;
+    this->readerThread->request_stop();
+    this->readerThread->join();
+}
+
+Response mmWave::sendCommand(std::vector<uint8_t> command)
+{
+    std::vector<uint8_t> dataToSend = COMMAND_HEADER;
+    dataToSend.push_back(command.size() & 0xFF);
+    dataToSend.push_back((command.size() >> 8) & 0xFF);
+    dataToSend.insert(dataToSend.end(), command.begin(), command.end());
+    dataToSend.insert(dataToSend.end(), COMMAND_TAIL);
+    serialPuts(this->serialPort_h, (char*)dataToSend.data());
+    Response response;
+    uint16_t waitTime = 0;
+    while(serialDataAvail(this->serialPort_h) == 0 && waitTime < 1000){
+        genericSleep(10);
+        waitTime += 10;
+    }
+    if(waitTime >= 1000){
+        CubeLog::error("Timeout trying to get response.");
+        response = false;
+        return response;
+    }
+    // These nested loops get the data fast, but if we read faster than the data arrives, give  the source
+    // time to catch up.
+    while(serialDataAvail(this->serialPort_h) > 0){
+        while(serialDataAvail(this->serialPort_h) > 0){
+            response += serialGetchar(this->serialPort_h);
+        }
+        genericSleep(10);
+    }
+    return response;
+}
+
+Response mmWave::sendCommand(std::vector<uint8_t> command, std::vector<uint8_t> ack)
+{
+    Response response = this->sendCommand(command);
+    if(!response.success || response != ack){
+        CubeLog::error("Failed to get expected response.");
+        response = false;
+        return response;
+    }
+    return response;
+}
+
+bool mmWave::enableConfigMode()
+{
+    std::vector<uint8_t> commandMode = COMMAND_HEADER;
+    std::vector<uint8_t> commandData = ENABLE_CONFIG_MODE;
+    commandMode.push_back(commandData.size() & 0xFF);
+    commandMode.push_back((commandData.size() >> 8) & 0xFF);
+    commandMode.insert(commandMode.end(), commandData.begin(), commandData.end());
+    commandMode.insert(commandMode.end(), COMMAND_TAIL);
+
+    std::vector<uint8_t> ack = COMMAND_HEADER;
+    std::vector<uint8_t> ackData = ENABLE_CONFIG_MODE_ACK;
+    ack.push_back(ackData.size() & 0xFF);
+    ack.push_back((ackData.size() >> 8) & 0xFF);
+    ack.insert(ack.end(), ackData.begin(), ackData.end());
+    ack.insert(ack.end(), COMMAND_TAIL);
+
+    Response response = this->sendCommand(commandMode, ack);
+    return response.success;
+}
+
+bool mmWave::disableConfigMode()
+{
+    std::vector<uint8_t> commandMode = COMMAND_HEADER;
+    std::vector<uint8_t> commandData = DISABLE_CONFIG_MODE;
+    commandMode.push_back(commandData.size() & 0xFF);
+    commandMode.push_back((commandData.size() >> 8) & 0xFF);
+    commandMode.insert(commandMode.end(), commandData.begin(), commandData.end());
+    commandMode.insert(commandMode.end(), COMMAND_TAIL);
+
+    std::vector<uint8_t> ack = COMMAND_HEADER;
+    std::vector<uint8_t> ackData = DISABLE_CONFIG_MODE_ACK;
+    ack.push_back(ackData.size() & 0xFF);
+    ack.push_back((ackData.size() >> 8) & 0xFF);
+    ack.insert(ack.end(), ackData.begin(), ackData.end());
+    ack.insert(ack.end(), COMMAND_TAIL);
+
+    Response response = this->sendCommand(commandMode, ack);
+    return response.success;
+}
+
+bool mmWave::enableEngineeringMode()
+{
+    std::vector<uint8_t> commandMode = COMMAND_HEADER;
+    std::vector<uint8_t> commandData = ENABLE_ENGINEERING_MODE;
+    commandMode.push_back(commandData.size() & 0xFF);
+    commandMode.push_back((commandData.size() >> 8) & 0xFF);
+    commandMode.insert(commandMode.end(), commandData.begin(), commandData.end());
+    commandMode.insert(commandMode.end(), COMMAND_TAIL);
+
+    std::vector<uint8_t> ack = COMMAND_HEADER;
+    std::vector<uint8_t> ackData = ENABLE_ENGINEERING_MODE_ACK;
+    ack.push_back(ackData.size() & 0xFF);
+    ack.push_back((ackData.size() >> 8) & 0xFF);
+    ack.insert(ack.end(), ackData.begin(), ackData.end());
+    ack.insert(ack.end(), COMMAND_TAIL);
+
+    Response response = this->sendCommand(commandMode, ack);
+    return response.success;
+}
+
+bool mmWave::disableEngineeringMode()
+{
+    std::vector<uint8_t> commandMode = COMMAND_HEADER;
+    std::vector<uint8_t> commandData = DISABLE_ENGINEERING_MODE;
+    commandMode.push_back(commandData.size() & 0xFF);
+    commandMode.push_back((commandData.size() >> 8) & 0xFF);
+    commandMode.insert(commandMode.end(), commandData.begin(), commandData.end());
+    commandMode.insert(commandMode.end(), COMMAND_TAIL);
+
+    std::vector<uint8_t> ack = COMMAND_HEADER;
+    std::vector<uint8_t> ackData = DISABLE_ENGINEERING_MODE_ACK;
+    ack.push_back(ackData.size() & 0xFF);
+    ack.push_back((ackData.size() >> 8) & 0xFF);
+    ack.insert(ack.end(), ackData.begin(), ackData.end());
+    ack.insert(ack.end(), COMMAND_TAIL);
+
+    Response response = this->sendCommand(commandMode, ack);
+    return response.success;
+}
+
+Response mmWave::readDataFrame()
+{
+    Response response;
+    response.data = REPORT_HEADER;
+    response = false;
+    if(this->commandModeEnabled){
+        CubeLog::error("Command mode enabled.");
+        return response;
+    }
+    uint16_t waitTime = 0;
+    while(serialDataAvail(this->serialPort_h) == 0 && waitTime < 1000){
+        genericSleep(10);
+        waitTime += 10;
+    }
+    if(waitTime >= 1000){
+        CubeLog::error("Timeout trying to get data frame.");
+        response = false;
+        return response;
+    }
+    if(serialDataAvail(this->serialPort_h) == 0){
+        CubeLog::error("No data available.");
+        response = false;
+        return response;
+    }
+    int headerIndex = 0;
+    while(serialDataAvail(this->serialPort_h) > 0){
+        uint8_t c = serialGetchar(this->serialPort_h);
+        if(c == response[headerIndex]){
+            headerIndex++;
+            if(headerIndex == response.size()){
+                break;
+            }
+        } else {
+            headerIndex = 0;
+        }
+    }
+    if(headerIndex != response.size()){
+        CubeLog::error("Failed to find report header.");
+        response = false;
+        return response;
+    }
+
+    uint16_t reportSize = 0;
+    reportSize = serialGetchar(this->serialPort_h);
+    reportSize |= serialGetchar(this->serialPort_h) << 8;
+
+    if(serialDataAvail(this->serialPort_h) < reportSize){
+        CubeLog::error("Report size does not match data available.");
+        response = false;
+        return response;
+    }
+
+    std::vector<uint8_t> tail = REPORT_TAIL;
+    for(uint16_t i = 0; i < reportSize + tail.size(); i++){
+        response += serialGetchar(this->serialPort_h);
+    }
+
+    if(response.size() != reportSize + tail.size()){
+        CubeLog::error("Failed to get full report.");
+        response = false;
+        return response;
+    }
+
+    for(uint16_t i = 0; i < tail.size(); i++){
+        if(response[reportSize + i] != tail[i]){
+            CubeLog::error("Failed to find report tail.");
+            response = false;
+            return response;
+        }
+    }
+
+    return response;
+}
+
+void mmWave::decodeDataFrame(Response response)
+{
+    if(!response.success){
+        CubeLog::error("Failed to decode data frame.");
+        return;
+    }
+    std::vector<uint8_t> data = response.data;
+    uint16_t index = 0;
+    uint16_t reportSize = data[index++];
+    reportSize |= data[index++] << 8;
+    uint16_t frameNumber = data[index++];
+    frameNumber |= data[index++] << 8;
+    uint16_t numDetectedObj = data[index++];
+    numDetectedObj |= data[index++] << 8;
+    uint16_t numTLVs = data[index++];
+    numTLVs |= data[index++] << 8;
+    CubeLog::info("Report size: " + std::to_string(reportSize));
+    CubeLog::info("Frame number: " + std::to_string(frameNumber));
+    CubeLog::info("Number of detected objects: " + std::to_string(numDetectedObj));
+    CubeLog::info("Number of TLVs: " + std::to_string(numTLVs));
+    for(uint16_t i = 0; i < numDetectedObj; i++){
+        uint16_t rangeIdx = data[index++];
+        rangeIdx |= data[index++] << 8;
+        uint16_t dopplerIdx = data[index++];
+        dopplerIdx |= data[index++] << 8;
+        uint16_t peakVal = data[index++];
+        peakVal |= data[index++] << 8;
+        uint16_t x = data[index++];
+        x |= data[index++] << 8;
+        uint16_t y = data[index++];
+        y |= data[index++] << 8;
+        uint16_t z = data[index++];
+        z |= data[index++] << 8;
+        uint16_t velocity = data[index++];
+        velocity |= data[index++] << 8;
+        uint16_t azimuth = data[index++];
+        azimuth |= data[index++] << 8;
+        uint16_t elevation = data[index++];
+        elevation |= data[index++] << 8;
+        CubeLog::info("Range index: " + std::to_string(rangeIdx));
+        CubeLog::info("Doppler index: " + std::to_string(dopplerIdx));
+        CubeLog::info("Peak value: " + std::to_string(peakVal));
+        CubeLog::info("X: " + std::to_string(x));
+        CubeLog::info("Y: " + std::to_string(y));
+        CubeLog::info("Z: " + std::to_string(z));
+        CubeLog::info("Velocity: " + std::to_string(velocity));
+        CubeLog::info("Azimuth: " + std::to_string(azimuth));
+        CubeLog::info("Elevation: " + std::to_string(elevation));
+    }
+    for(uint16_t i = 0; i < numTLVs; i++){
+        uint16_t tlvType = data[index++];
+        tlvType |= data[index++] << 8;
+        uint16_t tlvLength = data[index++];
+        tlvLength |= data[index++] << 8;
+        CubeLog::info("TLV type: " + std::to_string(tlvType));
+        CubeLog::info("TLV length: " + std::to_string(tlvLength));
+        for(uint16_t j = 0; j < tlvLength; j++){
+            CubeLog::info("TLV data: " + std::to_string(data[index++]));
+        }
+    }
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
-
-void generatePortNames(){
-#ifdef _WIN32
-    portPrefixes.push_back("COM");
-#else
-    portPrefixes.push_back("/dev/ttyACM"); 
-    portPrefixes.push_back("/dev/ttyUSB"); 
-    portPrefixes.push_back("/dev/ttyS");
-#endif
-    for(auto p : portPrefixes){
-        for(int i = 0; i < 50; i++){
-            portNames.push_back(p + std::to_string(i));
-        }
-    }
-}
--- src/hardware/mmWave.h
+++ src/hardware/mmWave.h
@@ -3,6 +3,7 @@
 #define MMWAVE_H
 #include <vector>
 #include <string>
+#include <memory>
 #include <logger.h>
 #ifdef __linux__
 #include <wiringPi.h>
@@ -15,8 +16,137 @@
 // TODO: add ifdefs for Windows so that this will compile on Windows
 // For windows, we should just mock the mmWave sensor.
 
+#define COMMAND_HEADER { 0xFD, 0xFC, 0xFB, 0xFA }
+#define COMMAND_TAIL { 0x04, 0x03, 0x02, 0x01 }
+#define REPORT_HEADER { 0xF4, 0xF3, 0xF2, 0xF1 }
+#define REPORT_TAIL { 0xF8, 0xF7, 0xF6, 0xF5 }
+
+#define ENABLE_CONFIG_MODE { 0xff, 0x00, 0x01, 0x00 }
+#define ENABLE_CONFIG_MODE_ACK { 0xff, 0x01, 0x00, 0x00, 0x01, 0x00, 0x40, 0x00 }
+#define DISABLE_CONFIG_MODE { 0xFE, 0x00 }
+#define DISABLE_CONFIG_MODE_ACK { 0xFE, 0x01, 0x00, 0x00 }
+
+#define ENABLE_ENGINEERING_MODE { 0x62, 0x00 }
+#define ENABLE_ENGINEERING_MODE_ACK { 0x62, 0x01, 0x00, 0x00 }
+#define DISABLE_ENGINEERING_MODE { 0x63, 0x00 }
+#define DISABLE_ENGINEERING_MODE_ACK { 0x63, 0x01, 0x00, 0x00 }
+
+class Response{
+public:
+    bool success = false;
+    std::vector<uint8_t> data = {};
+    std::string hexStr = "";
+
+    Response() = default;
+
+    size_t size() {
+        return data.size();
+    }
+
+    uint8_t operator[](const size_t& index) {
+        if(index < data.size()){
+            return data.at(index);
+        }
+        return 0;
+    }
+
+    bool operator==(const std::vector<uint8_t>& other) {
+        bool equal = false;
+        if(data.size() == other.size()){
+            equal = true;
+            for(size_t i = 0; i < data.size(); i++){
+                if(data[i] != other[i]){
+                    equal = false;
+                    break;
+                }
+            }
+        }
+        return equal;
+    }
+
+    bool operator!=(const std::vector<uint8_t>& other) {
+        return !(*this == other);
+    }
+
+    bool operator==(const bool& other) {
+        return success == other;
+    }
+
+    Response& operator=(const bool& newSuccess) {
+        success = newSuccess;
+        return *this;
+    }
+
+    Response& operator=(const std::vector<uint8_t>& newData) {
+        data = newData;
+        updateHexStr();
+        return *this;
+    }
+
+    Response& operator+=(const uint8_t& extraData) {
+        data.push_back(extraData);
+        updateHexStr();
+        return *this;
+    }
+
+    Response& operator+=(const char& extraData) {
+        data.push_back(static_cast<uint8_t>(extraData));
+        updateHexStr();
+        return *this;
+    }
+
+    // Addition operator
+    Response& operator+=(const std::vector<uint8_t>& extraData) {
+        data.insert(data.end(), extraData.begin(), extraData.end());
+        updateHexStr();
+        return *this;
+    }
+
+    // Addition operator for adding two Response objects' data together
+    Response& operator+=(const Response& other) {
+        data.insert(data.end(), other.data.begin(), other.data.end());
+        updateHexStr();
+        success = success && other.success;
+        return *this;
+    }
+
+    // Addition operator for creating a new Response object as the sum of two others
+    Response operator+(const Response& other) const {
+        Response result = *this;
+        result += other;
+        result.success = success && other.success;
+        return result;
+    }
+private:
+    // Helper function to convert vector data to hex string
+    void updateHexStr() {
+        std::ostringstream oss;
+        for (const auto& byte : data) {
+            oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
+        }
+        hexStr = oss.str();
+    }
+};
+
 class mmWave{
-    std::jthread* readerThread;
+    int serialPort_h;
+    bool commandModeEnabled = false;
+    std::unique_ptr<std::jthread> readerThread;
+    Response sendCommand(std::vector<uint8_t> command);
+    Response sendCommand(std::vector<uint8_t> command, std::vector<uint8_t> ack);
+    bool enableConfigMode();
+    bool disableConfigMode();
+    bool enableEngineeringMode();
+    bool disableEngineeringMode();
+    bool setMaxMovingDistanceGate(int distance);
+    bool setMaxRestingDistanceGate(int distance);
+    bool setUnmannedDuration(int duration);
+    bool setDistanceGateSensitivity(int sensitivity);
+    bool setMotionSensitivity(int sensitivity);
+    bool setRestingSensitivity(int sensitivity);
+    void restartModule();
+    Response readDataFrame();
+    void decodeDataFrame(Response response);
 public:
     mmWave();
     ~mmWave();
