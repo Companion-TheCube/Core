5910c5ba17225a7263e2cceb1e37faf40bb3ebf3
Andrew McDaniel <andrewmcdan@gmail.com>
2025-03-06 21:02:26 -0500

    Adjusting LilFlame Characters

--- src/gui/renderer.cpp
+++ src/gui/renderer.cpp
@@ -99,7 +99,8 @@ int Renderer::thread()
     this->textShader = &textureShader;
     this->stencilShader = &stencilShader;
     auto characterManager = new CharacterManager(&edgesShader);
-    Character_generic* character = characterManager->getCharacterByName("TheCube"); // TODO: this call should return a nullptr if the character is not found. Then we should throw an error.
+    // Character_generic* character = characterManager->getCharacterByName("TheCube"); // TODO: this call should return a nullptr if the character is not found. Then we should throw an error.
+    Character_generic* character = characterManager->getCharacterByName("LilFlame");
     characterManager->setCharacter(character);
     this->setupTasksRun();
     CubeLog::info("Renderer initialized. Starting Loop...");
