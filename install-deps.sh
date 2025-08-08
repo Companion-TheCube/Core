815d13d41da7c432f6deb58261bf9b13e33c1eb4
Andrew McDaniel <andrewmcdan@gmail.com>
2025-08-08 13:58:31 -0400

    refactor(deps): clean up install-deps.sh by removing commented sections and organizing dependency lists

--- install-deps.sh
+++ install-deps.sh
@@ -27,25 +27,18 @@ if [[ "$MODE" == "dev" ]]; then
     libglew-dev libfreetype6-dev libgl1-mesa-dev libglu1-mesa-dev \
     libasound2-dev libpulse-dev \
     libssl-dev libsodium-dev libsqlite3-dev libglm-dev libpq-dev gettext \
-    # Windowing/X11 headers used by SFML
     libx11-dev libxrandr-dev libxcursor-dev libxinerama-dev libxi-dev libudev-dev \
-    # SFML audio stack
-    libopenal-dev libsndfile1-dev libflac-dev libvorbis-dev libogg-dev
-    # Images and compression (used by SFML/graphics)
+    libopenal-dev libsndfile1-dev libflac-dev libvorbis-dev libogg-dev \
     libjpeg-dev libpng-dev zlib1g-dev
 else
   # Deployment (runtime) dependencies
   sudo apt-get install -y \
     ca-certificates \
-    # Core runtime libs
     libglew2.2 libfreetype6 libgl1 libglu1-mesa \
     libasound2 libpulse0 \
     libssl3 libsodium23 libsqlite3-0 libpq5 \
-    # Windowing/X11 runtime used by SFML
     libx11-6 libxrandr2 libxcursor1 libxinerama1 libxi6 libudev1 \
-    # SFML audio runtime codecs
-    libopenal1 libsndfile1 libflac8 libvorbis0a libvorbisfile3 libogg0
-    # Images and compression runtime
+    libopenal1 libsndfile1 libflac8 libvorbis0a libvorbisfile3 libogg0 \
     libjpeg-turbo8 libpng16-16 zlib1g
 fi
 
