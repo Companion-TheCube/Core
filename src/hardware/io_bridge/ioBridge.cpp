9ce04958e1357996c1f4de7b28b788beff305e54
Andrew McDaniel <andrewmcdan@gmail.com>
2025-08-22 21:41:42 -0400

    docs(ioBridge): update comments to clarify GPIO control for Raspberry Pi

--- src/hardware/io_bridge/ioBridge.cpp
+++ src/hardware/io_bridge/ioBridge.cpp
@@ -2,6 +2,8 @@
 
 The IO Bridge is an RP2354 IC connected via SPI that provides I2C, SPI, UART, GPIO, and other interfaces to the Cube.
 
+Though the IO Bridge has its own GPIO pins, we will also be controlling the Raspberry Pi's GPIO pins directly for certain functions.
+
 */
 
 #include "ioBridge.h"
